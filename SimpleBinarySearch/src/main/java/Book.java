a0ec706dea45663eb274a5c42bb5d5613014bba7
Alec Feldhaus <alec@mgmdesign.com>
2024-05-27 19:44:39 -0700

    First Commit

---
+++ SimpleBinarySearch/src/main/java/Book.java
@@ -0,0 +1,21 @@
+public class Book {
+    private int id;
+    private String name;
+
+    public Book(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String toString() {
+        return "(id: " + id + "; name: " + name + ")";
+    }
+}
